cd6256fc5d8021192034071a73df853491531efc
Sergioramos228 <serega170503@gmail.com>
2025-02-26 18:31:10 +0200

    #UI
    -Show History order

---
+++ Assets/Ballz + Merge/UI/HistoriView/ButtonToggle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+[Serializable]
+public class ButtonToggle
+{
+    public Button Toggle;
+    public TMP_Text Label;
+
+    private string _originalLabel;
+    private string _ascendingText;
+    private string _descendingText;
+    private bool _isAscending;
+
+    private Action<ButtonToggle> _triggered;
+
+    public void Initialize(string ascendingText, string descendingText, Action<ButtonToggle> onTrigger)
+    {
+        if (Label != null)
+            _originalLabel = Label.text;
+
+        if (Toggle != null)
+            Toggle.onClick.AddListener(AppendLabel);
+
+        _triggered = onTrigger;
+
+        _ascendingText = ascendingText;
+        _descendingText = descendingText;
+    }
+
+    public void Close()
+    {
+        if (Toggle != null)
+            Toggle.onClick.RemoveListener(AppendLabel);
+    }
+
+    public void ResetLabel()
+    {
+        if (Label != null)
+            Label.text = _originalLabel;
+    }
+
+    private void AppendLabel()
+    {
+        if (Label == null)
+            return;
+
+        _isAscending = !_isAscending;
+        Label.text = _isAscending ? $"{_originalLabel} {_ascendingText}" : $"{_originalLabel} {_descendingText}";
+        _triggered?.Invoke(this);
+    }
+}
--- Assets/Ballz + Merge/UI/HistoriView/GameHistoryView.cs
+++ Assets/Ballz + Merge/UI/HistoriView/GameHistoryView.cs
@@ -1,20 +1,27 @@
 ﻿using BallzMerge.Data;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class GameHistoryView : CyclicBehavior, IInitializable, IInfoPanelView
 {
     private const int CountPreload = 20;
 
-    [SerializeField] private Button _toggle;
-    [SerializeField] private TMP_Text _toggleLabel;
+    [SerializeField] private List<ButtonToggle> _toggles;
     [SerializeField] private GameDataView _gameDataPrefab;
     [SerializeField] private RectTransform _dataParent;
 
+    private Dictionary<ButtonToggle, Action<ButtonToggle>> _togglesDictionary = new();
     private string[] _toggleLabels = { "ID", "Date" };
+    private string[] _symbols = { "(↑)", "(↓)" };
     private int _index = 0;
+    private bool _isScoreAscending = true;
+    private bool _isNumberAscending = true;
+    private ButtonToggle _currentToggle;
     private List<GameHistoryData> _data;
     private List<GameDataView> _allViews = new List<GameDataView>();
     private RectTransform _rootParent;
@@ -22,7 +29,9 @@ public class GameHistoryView : CyclicBehavior, IInitializable, IInfoPanelView
 
     public void Show(RectTransform showcase)
     {
-        _toggle.onClick.AddListener(ChangeStateView);
+        foreach (var toggle in _togglesDictionary)
+            toggle.Key.Initialize(_symbols[0], _symbols[1], toggle.Value);
+
         gameObject.SetActive(true);
         _transform.SetParent(showcase, false);
     }
@@ -33,6 +42,9 @@ public class GameHistoryView : CyclicBehavior, IInitializable, IInfoPanelView
             return false;
 
         _data = data;
+        _togglesDictionary.Add(_toggles[0], ChangeStateView);
+        _togglesDictionary.Add(_toggles[1], OrderNumber);
+        _togglesDictionary.Add(_toggles[2], OrderScore);
 
         if (_data.Count > _allViews.Count)
             GenerateViews(_data.Count - _allViews.Count);
@@ -44,12 +56,14 @@ public class GameHistoryView : CyclicBehavior, IInitializable, IInfoPanelView
 
     public void Hide()
     {
-        foreach(var view in _allViews)
+        foreach (var view in _allViews)
             view.Hide();
 
         _transform.SetParent(_rootParent, false);
         gameObject.SetActive(false);
-        _toggle.onClick.RemoveListener(ChangeStateView);
+
+        foreach (var toggle in _togglesDictionary)
+            toggle.Key.Close();
     }
 
     public void Init()
@@ -72,11 +86,43 @@ public class GameHistoryView : CyclicBehavior, IInitializable, IInfoPanelView
             _allViews.Add(Instantiate(_gameDataPrefab, _dataParent).Init());
     }
 
-    private void ChangeStateView()
+    private void ChangeStateView(ButtonToggle buttonToggle)
     {
         _index = (_index + 1) % _toggleLabels.Length;
-        _toggleLabel.text = _toggleLabels[_index];
+        //_toggleLabel.text = _toggleLabels[_index];
+
+        if (_currentToggle != null)
+            _currentToggle.ResetLabel();
+
+        _currentToggle = buttonToggle;
         Show();
+    }
+
+    private void OrderScore(ButtonToggle buttonToggle)
+    {
+        ToggleSort(ref _isScoreAscending, x => x.Score);
+        SetNewLabel(buttonToggle);
+    }
+
+    private void OrderNumber(ButtonToggle buttonToggle)
+    {
+        ToggleSort(ref _isNumberAscending, x => x.Number);
+        SetNewLabel(buttonToggle);
+    }
+
+    private void ToggleSort<T>(ref bool ascending, Func<GameHistoryData, T> keySelector)
+    {
+        _data = ascending ? _data.OrderBy(keySelector).ToList() : _data.OrderByDescending(keySelector).ToList();
+        ascending = !ascending;
+        Show();
+    }
+
+    private void SetNewLabel(ButtonToggle buttonToggle)
+    {
+        if (_currentToggle != null && _currentToggle != buttonToggle)
+            _currentToggle.ResetLabel();
+
+        _currentToggle = buttonToggle;
     }
 
     private string GetData(GameHistoryData data)
